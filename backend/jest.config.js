32662292b33c0fc99cdb0a572a8ba89677ffdb01
Sreekuttan_S <skcollege007edu@gmail.com>
2026-09-05 14:06:09 +0530

    Delete backend directory

--- backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  rootDir: ".",
-  testMatch: ["<rootDir>/tests/**/*.test.ts"],
-  globalSetup: "<rootDir>/tests/globalSetup.ts",
-  testTimeout: 20000,
-};
--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "./app";
-import { env } from "./config/env";
-
-const app = createApp();
-
-app.listen(env.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`PeoplePay360 backend listening on port ${env.PORT} [${env.APP_ENV}]`);
-});
